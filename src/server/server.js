77c609d3c1d26a00d09ede3979689e43e55d9cea
Corey Hanson <chanson@barkbox.com>
2023-01-23 22:37:43 -0500

    slight cleanup

--- src/server/server.js
+++ src/server/server.js
@@ -22,7 +22,9 @@ app.use(webpackDevMiddleware(compiler, {
 
 const port = 3456
 
-app.use(cors({ origin: true}))
+const corsOptions = { credentials: true }
+app.use(cors(corsOptions));
+
 app.use(bodyParser.text())
 app.use(express.static(DIST_DIR))
 
@@ -37,30 +39,35 @@ app.get('/', (req, res, next) => {
   })
 })
 
-app.get('/tracking_code', (req, res) => {
+app.get('/tracking_code', (_req, res) => {
   res.send('!function(c,l,a,r,i,t,y){if(a[c].v||a[c].t)return a[c]("event",c,"dup."+i.projectId);a[c].t=!0,(t=l.createElement(r)).async=!0,t.src="https://www.clarity.ms/eus-b/s/0.7.1/clarity.js",(y=l.getElementsByTagName(r)[0]).parentNode.insertBefore(t,y),a[c]("start",i),a[c].q.unshift(a[c].q.pop())}("clarity",document,window,"script",{"projectId":"f579s7kgqn","upload":"http://localhost:3456/clarity_upload","expire":365,"cookies":["_uetmsclkid","_uetvid"],"track":true,"lean":false,"content":true,"extract":[3,1,["4f509md79","zjy56jaz","4s4zm72hj"]]});')
 })
 
 
 app.post('/clarity_upload', cors({ origin: true}), (req, res) => {
-  if(req.body != {}) {
+  if(req.body.constructor === Object && Object.keys(req.body).length === 0) {
+    console.log('not uploading')
+    res.sendStatus(200);
+  } else {
     const rawEvent = req.body;
     const decodedEvent = decode(rawEvent);
+    console.log(decodedEvent)
 
     const cs = new pgp.helpers.ColumnSet(['timestamp', 'raw_event', 'decoded_event', 'user_id'], { table: 'clarity_events' });
     const values = [{
-      timestamp: decodedEvent.timestamp, raw_event: JSON.stringify(rawEvent), decoded_event: JSON.stringify(decodedEvent), user_id: decodedEvent.envelope.userId
+      timestamp: decodedEvent.timestamp,
+      raw_event: JSON.stringify(rawEvent),
+      decoded_event: JSON.stringify(decodedEvent),
+      user_id: decodedEvent.envelope.userId
     }]
     const query = pgp.helpers.insert(values, cs);
     db.none(query);
 
     res.sendStatus(200);
-  } else {
-    res.sendStatus(200);
   }
 })
 
-app.get('/db_fetch', (req, res) => {
+app.get('/db_fetch', (_req, res) => {
   db.any("SELECT * FROM clarity_events;")
   .then((data) => {
     console.log('DATA:', data)
@@ -72,7 +79,7 @@ app.get('/db_fetch', (req, res) => {
   })
 })
 
-app.get('/visualize', (req, res, next) => {
+app.get('/visualize', (_req, res, next) => {
   compiler.outputFileSystem.readFile(path.join(DIST_DIR, 'visualize.html'), (err, result) => {
     if (err) {
       return next(err)
